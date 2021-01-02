976f60e47a4b6f00eef0b8202edb6fbe6906820b
Arman Abkar <armanw@outlook.com>
2021-01-03 00:02:10 +0330

    initial commit

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MissionKey() {
+  return (
+    <div className="mission-keys">
+      <p className="font-weight-bold success">Success</p>
+      <p className="font-weight-bold fail ml-2">Fail</p>
+    </div>
+  );
+}
